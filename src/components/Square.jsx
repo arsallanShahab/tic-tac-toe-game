f5158cdd8e8a51330f366312fb65bd406ea9dbed
Arsallan Shahab <mdarsallan@gmail.com>
2022-08-07 00:37:31 +0530

    added sass dependency

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Square = props => {
-  return <button>{props.value}</button>;
+const Square = ({ value }) => {
+  return <button type="button">{value}</button>;
 };
 
 export default Square;
