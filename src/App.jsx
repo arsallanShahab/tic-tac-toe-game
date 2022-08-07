6c948ece9414d6cbda5b1a59a1cb9e2602fbab04
Arsallan Shahab <mdarsallan@gmail.com>
2022-08-07 19:07:50 +0530

    just refracted the code only

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './components/Board';
 import './styles/root.scss';
 
-const App = () => (
-  <div className="app">
-    <h1 className="heading">Tic Tac Toe </h1>
-    <Board />
-  </div>
-);
+const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setXNext] = useState(true);
+
+  const handleSquareClick = position => {
+    if (board[position]) {
+      return;
+    } else {
+      setBoard(prev => {
+        return prev.map((square, pos) => {
+          if (pos === position) {
+            return isXNext ? 'X' : 'O';
+          }
+          return square;
+        });
+      });
+      setXNext(prev => {
+        return !prev;
+      });
+    }
+  };
+
+  return (
+    <div className="app">
+      <h1 className="heading">Tic Tac Toe </h1>
+      <Board board={board} handleSquareClick={handleSquareClick} />
+    </div>
+  );
+};
 
 export default App;
