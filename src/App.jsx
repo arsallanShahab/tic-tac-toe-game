c614a095f3c337071e27ce0e44767c2535f4acae
Arsallan Shahab <mdarsallan@gmail.com>
2022-08-07 20:49:04 +0530

    added game histroy

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,44 @@ import calculateWinner from './Helpers';
 import './styles/root.scss';
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setXNext] = useState(true);
+  const [histroy, setHistroy] = useState([
+    { board: Array(9).fill(null), isNext: true },
+  ]);
 
-  const winner = calculateWinner(board);
+  const [currentMove, setCurrentMove] = useState(0);
+  const current = histroy[currentMove];
+
+  console.log(histroy);
+
+  const winner = calculateWinner(current.board);
   const message = winner
     ? `The winner is ${winner}`
-    : `The next player is  ${isXNext ? 'X' : 'O'}`;
+    : `The next player is  ${current.isNext ? 'X' : 'O'}`;
 
   const handleSquareClick = position => {
-    if (board[position] || winner) {
+    if (current.board[position] || winner) {
       return;
     } else {
-      setBoard(prev => {
-        return prev.map((square, pos) => {
+      setHistroy(prev => {
+        const last = prev[prev.length - 1];
+        const newBoard = last.board.map((square, pos) => {
           if (pos === position) {
-            return isXNext ? 'X' : 'O';
+            return last.isNext ? 'X' : 'O';
           }
           return square;
         });
-      });
-      setXNext(prev => {
-        return !prev;
+
+        return prev.concat({ board: newBoard, isNext: !last.isNext });
       });
     }
+    setCurrentMove(prev => prev + 1);
   };
 
   return (
     <div className="app">
       <h1 className="heading">Tic Tac Toe </h1>
       <h2>{message}</h2>
-      <Board board={board} handleSquareClick={handleSquareClick} />
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
     </div>
   );
 };
