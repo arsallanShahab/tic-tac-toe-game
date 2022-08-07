bb5dea232ef2ebdc37fcd6f42881d65429b79cb6
Arsallan Shahab <mdarsallan@gmail.com>
2022-08-07 19:29:10 +0530

    added winner function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
+import calculateWinner from './Helpers';
 import './styles/root.scss';
 
 const App = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [isXNext, setXNext] = useState(true);
 
+  const winner = calculateWinner(board);
+  const message = winner
+    ? `The winner is ${winner}`
+    : `The next player is  ${isXNext ? 'X' : 'O'}`;
+
   const handleSquareClick = position => {
-    if (board[position]) {
+    if (board[position] || winner) {
       return;
     } else {
       setBoard(prev => {
@@ -27,7 +33,7 @@ const App = () => {
   return (
     <div className="app">
       <h1 className="heading">Tic Tac Toe </h1>
-      <h2>message</h2>
+      <h2>{message}</h2>
       <Board board={board} handleSquareClick={handleSquareClick} />
     </div>
   );
